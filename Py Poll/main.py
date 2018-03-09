adb846b7cb2ea00aa893a5c02cae514227301cd1
ehyoder <ehyoder@gmail.com>
2018-03-08 21:41:54 -0500

    This is an update to Python Challenge

---
+++ Py Poll/main.py
@@ -0,0 +1,95 @@
+# Dependencies 
+import csv
+import os
+import sys
+
+# Files to load 
+election1_csv = os.path.join("election_data_1.csv")
+election2_csv = os.path.join("election_data_2.csv")
+
+# Output file
+output_file = "new_election_data.txt" 
+
+# Lists and dictionaries to store data 
+Candidates = []
+Candidate_votes = {}
+Percentage_won = []
+Total_votes = []
+Total_votes = 0 
+
+# Open the first file and read as a dictionary
+with open(election1_csv, 'r') as csvfile:
+    csvreader= csv.DictReader(csvfile, delimiter= ',')
+
+    # Iterate over each row to find data
+    for row in csvreader:
+        Total_votes = Total_votes + 1
+
+        # Assign candidate_name as key
+        Candidate_name = row["Candidate"]
+        # If a candidate's name is not in the list of candidates, add it to that list
+        if Candidate_name not in Candidates: 
+            Candidates.append(Candidate_name)
+            Candidate_votes[Candidate_name] = 0
+
+        # Track the votes for each candidate
+        Candidate_votes[Candidate_name] = Candidate_votes[Candidate_name] + 1
+
+Election_results1
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {Total_votes}\n"
+      f"--------------------------")
+    
+# Find the percentage of the vote that each candidate won, print to terminal
+for candidate in Candidate_votes:
+    Votes = Candidate_votes.get(candidate)
+    Vote_percentage = float(Votes) / float(Total_votes) * 100
+    print(f"{candidate}: {Vote_percentage: .1f}% ({Votes})\n")
+    
+# Find the winner and output to terminal
+winner = max(Candidate_votes, key=Candidate_votes.get)
+print("--------------------------")
+print(f"Winner: {winner}\n"
+      f"--------------------------\n")
+
+################################################# SECOND FILE #####################################################
+
+# Open the second file and read as a dictionary
+with open(election2_csv, 'r') as csvfile:
+    csvreader= csv.DictReader(csvfile, delimiter= ',')
+
+    # Iterate over each row to find data
+    for row in csvreader:
+        Total_votes = Total_votes + 1
+
+        # Assign candidate_name as key
+        Candidate_name = row["Candidate"]
+        # If a candidate's name is not in the list of candidates, add it to that list
+        if Candidate_name not in Candidates: 
+            Candidates.append(Candidate_name)
+            Candidate_votes[Candidate_name] = 0
+
+        # Track the votes for each candidate
+        Candidate_votes[Candidate_name] = Candidate_votes[Candidate_name] + 1
+
+print("Election Results 2")
+print("--------------------------")
+print(f"Total Votes: {Total_votes}\n"
+      f"--------------------------")
+    
+# Find the percentage of the vote that each candidate won, print to terminal
+for candidate in Candidate_votes:
+    Votes = Candidate_votes.get(candidate)
+    Vote_percentage = float(Votes) / float(Total_votes) * 100
+    print(f"{candidate}: {Vote_percentage: .1f}% ({Votes})\n")
+    
+# Find the winner and output to terminal
+winner = max(Candidate_votes, key=Candidate_votes.get)
+print("--------------------------")
+print(f"Winner: {winner}\n"
+      f"--------------------------")
+
+# Export the election results to the text file
+sys.stdout = open(output_file, 'w')
+print(election_results1)
--- Py Poll/pypoll_beth_yoder.py
+++ Py Poll/pypoll_beth_yoder.py
@@ -5,7 +5,7 @@ import sys
 
 # Files to load 
 election1_csv = os.path.join("election_data_1.csv")
-#election2_csv = os.path.join("election_data_2.csv")
+election2_csv = os.path.join("election_data_2.csv")
 
 # Output file
 output_file = "new_election_data.txt" 
@@ -44,7 +44,45 @@ print(f"Total Votes: {Total_votes}\n"
 for candidate in Candidate_votes:
     Votes = Candidate_votes.get(candidate)
     Vote_percentage = float(Votes) / float(Total_votes) * 100
-    print(f"{candidate}: {Vote_percentage: .3f}% ({Votes})\n")
+    print(f"{candidate}: {Vote_percentage: .1f}% ({Votes})\n")
+    
+# Find the winner and output to terminal
+winner = max(Candidate_votes, key=Candidate_votes.get)
+print("--------------------------")
+print(f"Winner: {winner}\n"
+      f"--------------------------\n")
+
+
+# Open the second file and read as a dictionary
+with open(election2_csv, 'r') as csvfile:
+    csvreader= csv.DictReader(csvfile, delimiter= ',')
+
+    # Iterate over each row to find data
+    for row in csvreader:
+        Total_votes = Total_votes + 1
+
+        # Assign candidate_name as key
+        Candidate_name = row["Candidate"]
+        # If a candidate's name is not in the list of candidates, add it to that list
+        if Candidate_name not in Candidates: 
+            Candidates.append(Candidate_name)
+            Candidate_votes[Candidate_name] = 0
+
+        # Track the votes for each candidate
+        Candidate_votes[Candidate_name] = Candidate_votes[Candidate_name] + 1
+
+Election_results_2 = (
+    f"Election Results 2"
+    f"--------------------------"
+    f"Total Votes: {Total_votes}\n"
+    f"--------------------------")
+print(Election_results_2, end='\n')
+    
+# Find the percentage of the vote that each candidate won, print to terminal
+for candidate in Candidate_votes:
+    Votes = Candidate_votes.get(candidate)
+    Vote_percentage = float(Votes) / float(Total_votes) * 100
+    print(f"{candidate}: {Vote_percentage: .1f}% ({Votes})\n")
     
 # Find the winner and output to terminal
 winner = max(Candidate_votes, key=Candidate_votes.get)
@@ -52,7 +90,6 @@ print("--------------------------")
 print(f"Winner: {winner}\n"
       f"--------------------------")
 
-    
 # Export the election results to the text file
-#sys.stdout = open(output_file, 'w')
+sys.stdout = open(output_file, 'w')
 #print(election_results1)
